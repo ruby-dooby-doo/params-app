1ec742c46fc079470509a088c0d941664d74f6f8
Brian <riceb53@gmail.com>
2018-06-27 19:39:19 -0500

    make query_guessing game

--- app/controllers/api/parameters_example_controller.rb
+++ app/controllers/api/parameters_example_controller.rb
@@ -13,4 +13,19 @@ class Api::ParametersExampleController < ApplicationController
     end
     render "query_params_name.json.jbuilder"
   end
+
+  def query_game
+    # if the user guesses correctly, tell them good job
+    if params["user_guess"].to_i == 36
+      @message = "Congratulations, you won!"
+    elsif params["user_guess"].to_i < 36
+    # if they are too low, tell them to guess higher
+      @message = "You did terribly, guess higher next time dummy"
+    else
+      # if they guess too high, tell them to guess lower
+      @message = "You did terribly, guess lower next time dummy"
+    end
+
+    render "query_game_view.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/query_parameter_url' => 'parameters_example#query_params_action'
     get '/query_parameter_name' => 'parameters_example#query_params_name'
+    get '/query_guessing_game' => 'parameters_example#query_game'
   end
 end
